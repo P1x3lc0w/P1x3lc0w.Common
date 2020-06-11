8cb75f63ffd986677c3ea10453e3b9b973602ed0
p1x3lc0w <p1x3lc0w@protonmail.com>
2020-06-11 14:32:32 +0200

    Initial commit

---
+++ P1x3lc0w.Common/ComparalbeKeyValuePair.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace P1x3lc0w.Common
+{
+    public struct ComparalbeKeyValuePair<TKey, TValue> : IComparable<TKey>, IComparable<ComparalbeKeyValuePair<TKey, TValue>> where TKey : IComparable<TKey>
+    {
+        public TKey key;
+        public TValue value;
+
+        public ComparalbeKeyValuePair(TKey key, TValue value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int CompareTo([AllowNull] TKey other) => key.CompareTo(other);
+
+        public int CompareTo([AllowNull] ComparalbeKeyValuePair<TKey, TValue> other) => key.CompareTo(other.key);
+    }
+}
