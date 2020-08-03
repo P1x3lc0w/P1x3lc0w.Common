180208edb5830d1510b8d1ec6e22f5b5a0da31d4
p1x3lc0w <p1x3lc0w@protonmail.com>
2020-08-03 13:11:01 +0200

    Add IProvider<T>

---
+++ P1x3lc0w.Common/IProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P1x3lc0w.Common
+{
+    interface IProvider<T>
+    {
+        public T Get();
+    }
+}
---
+++ P1x3lc0w.Common/SimpleProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P1x3lc0w.Common
+{
+    class SimpleProvider<T> : IProvider<T>
+    {
+        public T ProvidedObject { get; protected set; }
+
+        public SimpleProvider(T providedObject)
+        {
+            ProvidedObject = providedObject;
+        }
+        public T Get() => ProvidedObject;
+
+        public static implicit operator SimpleProvider<T>(T obj) 
+            => new SimpleProvider<T>(obj);
+    }
+}
